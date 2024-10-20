d976ae02a3838915f551940d92e9401b9db84f33
James Moses <jamxsmosxs@gmail.com>
2024-10-20 20:26:32 +0100

    Added initial to-do list app files

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    console.log(taskText);
+    if (taskText === "") {
+      alert("Input field cannot be empty. Enter a text");
+    } else {
+      const newElement = document.createElement("li");
+      newElement.textContent = taskText;
+      const removeBtn = document.createElement("button");
+      removeBtn.textContent = "Remove";
+      removeBtn.addEventListener("click", () => {
+        newElement.remove();
+      });
+      newElement.appendChild(removeBtn);
+      taskList.appendChild(newElement);
+      taskInput.value = "";
+    }
+  }
+
+  addButton.addEventListener("click", addTask);
+});
